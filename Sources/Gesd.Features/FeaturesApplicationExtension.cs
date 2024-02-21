518c53e6d25f43363b1e1dd850d79a4c6e43831a
Sillery <tsillery@ymail.com>
2024-02-21 01:10:10 -0500

    Ajout des extensions et creation des dependances de projets

---
+++ Sources/Gesd.Features/FeaturesApplicationExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesd.Features
+{
+    public static class FeaturesApplicationExtension
+    {
+        public static IServiceCollection AddFeaturesConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ Sources/Gesd.Ioc/IocApplicationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Gesd.Application;
+using Gesd.Data;
+using Gesd.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gesd.Ioc
+{
+    public static  class IocApplicationExtensions
+    {
+        public static IServiceCollection AddIocConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddApplicationServiceConfiguration(configuration);
+            services.AddDataConfiguration(configuration);
+            services.AddFeaturesConfiguration(configuration);
+            return services;
+        }
+    }
+}
